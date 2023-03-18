87d556c9d9c1c780b9265fa95f4a6e5566d52a3c
Arjesh <arjesh.khadka2@gmail.com>
2023-03-18 19:45:20 +1100

    project section added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Hero from './components/Hero';
 import Skills from './components/Skills';
+import Projects from './components/Projects';
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
       <Hero/>
 
       <Skills/>
+
+      <Projects/>
      
     </div>
   );
---
+++ src/components/Projects.js
@@ -0,0 +1,156 @@
+import React from 'react'
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/pic.png";
+import projImg2 from "../assets/img/pic.png";
+import projImg3 from "../assets/img/pic.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+const Projects = () => {
+    const projects = [
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg1,
+        },
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg2,
+        },
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg3,
+        },
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg1,
+        },
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg2,
+        },
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg3,
+        },
+      ];
+
+
+      const project2 = [
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg1,
+        },
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg2,
+        },
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg3,
+        },
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg1,
+        },
+        
+      ];
+
+      const project3 = [
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg1,
+        },
+        
+        
+      ];
+
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p>As a developer working on a project that utilizes HTML, CSS, JavaScript, React, and TypeScript, I'm excited about the potential to create a dynamic and modern web application. I have used React as the front-end framework and TypeScript for added type safety. Overall, I'm looking forward to learn more of these technologies to build a successful project.</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">React</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">HTML/CSS/JS</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">TypeScript</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">
+                    <Row>
+                        {
+                          project2.map((project2, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project2}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                    <Row>
+                        {
+                          project3.map((project3, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project3}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      
+    </section>
+  )
+}
+
+export default Projects
